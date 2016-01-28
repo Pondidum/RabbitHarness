77e70c843681da0f0a70717e0db45260cf617985
andy <andy@andydote.co.uk>
2016-01-28 21:07:44 +0000

    tests: add scratchpad and requiresRabbitFact attribute

---
+++ RabbitHarness.Tests/RequiresRabbitFactAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RabbitMQ.Client;
+using Xunit;
+
+namespace RabbitHarness.Tests
+{
+	public class RequiresRabbitFactAttribute : FactAttribute
+	{
+		private static Func<bool> _isAvailable; 
+
+		public RequiresRabbitFactAttribute(string host)
+		{
+			_isAvailable = _isAvailable ?? (() =>
+			{
+				var factory = new ConnectionFactory
+				{
+					HostName = host,
+					RequestedConnectionTimeout = 1000
+				};
+
+				try
+				{
+					using (var connection = factory.CreateConnection())
+					{
+						return connection.IsOpen;
+					}
+				}
+				catch (Exception)
+				{
+					return false;
+				}
+			});
+
+			if (_isAvailable() == false)
+			{
+				Skip = "RabbitMQ is not available";
+			}
+		}
+	}
+}
