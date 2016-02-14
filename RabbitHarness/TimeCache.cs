13a6b95e1e038c5ef507c10a7ef0f97c960bdfae
andy <andy@andydote.co.uk>
2016-02-14 20:32:06 +0000

    timeCache: add simple cache of ntp time

---
+++ RabbitHarness/TimeCache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using RabbitMQ.Client;
+
+namespace RabbitHarness
+{
+	public class TimeCache
+	{
+		private static readonly Lazy<DateTime> Start;
+		private static readonly Stopwatch Stopwatch;
+
+		static TimeCache()
+		{
+			Stopwatch = new Stopwatch();
+			Start = new Lazy<DateTime>(() =>
+			{
+				var nt = new NetworkTime();
+				Stopwatch.Start();
+				return nt.GetNetworkTime();
+			});
+		}
+
+		public static AmqpTimestamp Now()
+		{
+			var now = Start.Value.AddMilliseconds(Stopwatch.ElapsedMilliseconds);
+
+			return new AmqpTimestamp((long)(now - NetworkTime.Epoch).TotalSeconds);
+		}
+	}
+}
