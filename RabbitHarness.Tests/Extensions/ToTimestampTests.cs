c8548fe62717af26112f33fa6bf2ba78310a6cd5
andy <andy@andydote.co.uk>
2016-03-11 20:16:15 +0000

    extensions: add toTimestamp and toDatetime methods

---
+++ RabbitHarness.Tests/Extensions/ToTimestampTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace RabbitHarness.Tests.Extensions
+{
+	public class ToTimestampTests
+	{
+		private readonly ITestOutputHelper _output;
+
+		public ToTimestampTests(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Theory]
+		[InlineData("2016/03/11 20:07:39")]
+		[InlineData("1970/01/01 00:00:00")]
+		public void When_converting(string datetime)
+		{
+			var dt = DateTime.Parse(datetime);
+
+			dt.ToTimestamp().ToDateTime().ShouldBe(dt);
+		}
+	}
+}
--- RabbitHarness/Extensions.cs
+++ RabbitHarness/Extensions.cs
@@ -9,6 +9,22 @@ namespace RabbitHarness
 {
 	public static class Extensions
 	{
+
+		private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+		public static DateTime ToDateTime(this AmqpTimestamp self)
+		{
+			return Epoch.AddSeconds(self.UnixTime).ToLocalTime();
+		}
+
+		public static AmqpTimestamp ToTimestamp(this DateTime self)
+		{
+			var delta = self.ToUniversalTime() - Epoch;
+			var ticks = (long)delta.TotalSeconds;
+
+			return new AmqpTimestamp(ticks);
+		}
+
 		public static void Query<TResponse>(this ConnectionFactory factory, string queueName, object message, Action<ResponseContext<TResponse>> callback)
 		{
 			var context = new QueryContext
