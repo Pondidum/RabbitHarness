3f9333c1db83f633e711645374107ebf6745587e
andy <andy@andydote.co.uk>
2016-05-02 15:04:34 +0100

    exchangeDefinition: exchanges must have a name and type, so use ctor params

--- RabbitHarness/ExchangeDefinition.cs
+++ RabbitHarness/ExchangeDefinition.cs
@@ -5,11 +5,21 @@ namespace RabbitHarness
 {
 	public class ExchangeDefinition
 	{
-		public string Name { get; set; }
-		public string Type { get; set; }
+		public string Name { get; protected set; }
+		public string Type { get; protected set; }
 		public bool AutoDelete { get; set; }
 		public bool Durable { get; set; }
 		public IDictionary<string, object> Args { get; set; }
+
+		protected ExchangeDefinition()
+		{
+		}
+
+		public ExchangeDefinition(string name, string type)
+		{
+			Name = name;
+			Type = type;
+		}
 
 		public virtual void Declare(IModel channel)
 		{
