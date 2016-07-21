7ddfcf7e07cbf5ee8c3bf11f52097bfcb854dd93
andy <andy@andydote.co.uk>
2016-07-21 09:16:42 +0100

    tests: add tests to prove containers can build RabbitConnector correctly

---
+++ RabbitHarness.Tests/ContainerTests.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using Shouldly;
+using StructureMap;
+using StructureMap.Graph;
+using Xunit;
+
+namespace RabbitHarness.Tests
+{
+	public class ContainerTests : TestBase
+	{
+		[Fact]
+		public void When_constructed_by_structuremap_with_no_special_config()
+		{
+			var container = new Container(c =>
+			{
+				c.For<ConnectionFactory>().Use(Factory);
+				c.Scan(a =>
+				{
+					a.TheCallingAssembly();
+					a.AssemblyContainingType<IRabbitConnector>();
+					a.WithDefaultConventions();
+				});
+			});
+
+			//if the wrong ctor was called, the factory would be null, and throw a null-ref exception
+			Should.NotThrow(() =>
+			{
+				var connector = container.GetInstance<RabbitConnector>();
+
+				var unsub = connector.ListenTo(
+					new QueueDefinition { Name = QueueName, AutoDelete = true },
+					new LambdaMessageHandler<int>((props, message) => true));
+
+				unsub();
+			});
+		} 
+	}
+}
