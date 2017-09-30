5b0bbdaaebb8f6b1171ea155ac73d3197f348368
andy <andy@andydote.co.uk>
2017-09-30 22:15:22 +0300

    build: start a docker container of rabbit if docker available

--- build.sh
+++ build.sh
@@ -5,9 +5,18 @@
 MODE=${1:-Debug}
 NAME="RabbitHarness"
 
+if [ -x "$(command -v docker)" ]; then
+  CONTAINER=$(docker run -d --rm -p 5672:5672 rabbitmq:3.6.11-management-alpine)
+  echo "Started RabbitMQ container: $CONTAINER"
+  sleep 2
+fi
+
 dotnet restore "$NAME.sln"
 dotnet build "$NAME.sln" --configuration $MODE
 
 find . -iname "*.Tests.csproj" -type f -exec dotnet test "{}" --configuration $MODE \;
-
 dotnet pack ./src/$NAME --configuration $MODE --output ../../.build
+
+if [ -x "$(command -v docker)" ]; then
+  docker stop $CONTAINER
+fi
